f86adf648ddd08f64e8956b01d188cb3884d6533
KickOfSilver <pedrolucasdesouzamacedo@gmail.com>
2023-04-05 22:49:42 -0300

    renamed file

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from discord import app_commands
 from bot.functions.MyBot.profile import set_bot_activity
 from bot.functions.commands.check import check_command_message
 from bot.functions.commands.registration import update_slash_commands
-from bot.commands.play import music_search
+from bot.commands.search import music_search
 
 # Carregando as informações de configuração do arquivo config.json
 with open("config.json") as f:
@@ -62,7 +62,7 @@ async def on_message(message):
 # Definição do comando play na árvore de comandos
 @client.tree.command(name="play", description="consulta")
 async def play_music(interaction, musica: str):
-    message = interaction.data['options'][0]['value']   
+    message = interaction.data['options'][0]['value']
     print(message)
     # await music_search(client, message)
 
